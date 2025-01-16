11c4e9ff775aa8732378f1261c03891402ce5021
Sung <25SungC@badinhs.org>
2025-01-16 13:42:44 -0500

    Took some notes about numbers

--- variableNotes.js
+++ variableNotes.js
@@ -2,5 +2,46 @@
 // We will use a commination of commets and code to build up our notes
 
 // We start by declaring a variable
-let myName = "Jeff";
-myName;
+// let myName = "Jeff";
+// console.log(myName);
+// myName = "Steve";
+// console.log(myName);
+
+//Here is a different way to declare a variable:
+// const e = 2.71828;
+// console.log(e);
+// e = 3.1415
+// console.log(e);
+// ^^^ This doesn't work, because you can't change a variable
+// A Third way to create a variable:
+// var pi = 3.1415
+// console.log(pi);
+// pi = e;
+// console.log(pi);
+// let name = "Ligma";
+// console.log(name);
+// // Var and Let are pretty inchangeable, but const makes variables that can't be changed
+// let total = 0;
+// let i = 0;
+// while (i <1001){
+//     total = total + i;
+//     i = i + 1;
+// }
+// console.log(total)
+
+// We'll Start looking at numbers and math below;
+// JavaScript can Add
+console.log(23 + 97);
+// You can store values in variables
+let sumOfSix = 420 + 69 + 69420 + 8 + 7 + 42069;
+console.log(sumOfSix);
+
+// you can do math in the console including using variables from
+// the javascript file you ran
+//Here is a simulation of calculating percent error
+const actual = 57;
+let prediction =  actual -13;
+let percentage = prediction / actual;
+console.log(percentage);
+let percentError = 1 -percentage;
+console.log(percentError);
