729adf61b4187904d6a1d005149cb2704428d903
Sung <25SungC@badinhs.org>
2025-02-12 13:41:54 -0500

    First part of notes added

--- LoopsNotes.js
+++ LoopsNotes.js
@@ -1,1 +1,23 @@
 /* Notes on loops in javaScript */
+
+/* Loops are a great way for us to repeat a set of steps.
+If you have something you want to do multiple times in a row, or 
+until somethings happens, loops are an ideal solution
+
+There are two general flavors of loop in programming:
+-While loop
+-For loops
+
+WHILE LOOPS:
+while (condition) {
+    //CODE GOES HERE
+}
+
+This code will repeat as long as that condition is true.
+
+example: */
+let i = 0
+while (i < 3){
+    console.log(i);
+    i++; // Short hand for i += 1, which is in turn a shorthand for i = i + 1
+}
