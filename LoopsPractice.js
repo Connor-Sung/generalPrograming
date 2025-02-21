686cba6684c82989c2b27ec74458b0b4c4d1e56d
Sung <25SungC@badinhs.org>
2025-02-21 13:43:41 -0500

    Finished 1-4 of Pracetices

---
+++ LoopsPractice.js
@@ -0,0 +1,41 @@
+// 1
+// for (let i = 0; i <= 21; i++) { 
+//     console.log(i);
+// }
+
+// 2
+
+// let i = 0
+// do { 
+//     (i++); 
+//     alert( `number ${i}!` ); 
+// } while (i < 3);
+
+// 3
+
+// let sum = 0;
+// while(true){
+//     let value = +prompt("Enter a number", '');
+
+//      if(value > 100){
+//          break;
+//      }
+// }
+
+// 4 
+
+// let sum2 = 0;
+// while(true){
+//     let value = +prompt("Enter a number", '');
+
+//     if(!value){
+//         break;
+//     }
+//     sum2 += value;
+// }
+// console.log(`sum = ` + sum2);
+
+// 5
+
+let Musics = ["Jazz", "Blues"]
+Musics.unshift()
